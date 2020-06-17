ee0fa084aceaccd59167bd3ef686f0395ac92b5f
Shubh Arya <56305668+shubh-learner@users.noreply.github.com>
2020-06-18 01:33:10 +0530

    debugger on

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def predict():
     	return render_template('result.html', prediction=my_prediction)
 
 if __name__ == '__main__':
-	#app.run(debug=True)
-    app.run (host = '0.0.0.0', port = 5000)
+	app.run(debug=True)
+   
